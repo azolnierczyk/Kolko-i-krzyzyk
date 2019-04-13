fa10e640686a4f31a5d5741ca67d9a9a2459a1fa
azolnierczyk <arek.zolnierczyk@gmail.com>
2019-04-13 13:23:19 +0200

    Pierwsza wersja kodu

--- Kolko i krzyzyk/Program.cs
+++ Kolko i krzyzyk/Program.cs
@@ -10,6 +10,22 @@ namespace Kolko_i_krzyzyk
     {
         static void Main(string[] args)
         {
+            string ImieGraczaA = "";
+            string ImieGraczaB = "";
+            char ZnakGraczaA = 'x';
+            char ZnakGraczaB = 'o';
+            char[,] plansza = new char[3, 3] {
+                { '1', '2', '3' },
+                { '4', '5', '6' },
+                { '7', '8', '9' }
+                };
+
+            Console.Write("Wpisz imie gracza A: ");
+            ImieGraczaA = Console.ReadLine();
+            Console.Write("Wpisz imie gracza B: ");
+            ImieGraczaB = Console.ReadLine();
+
+            Console.ReadKey();
         }
     }
 }
